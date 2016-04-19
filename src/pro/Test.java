7e475c0ce51050ea761cae01b59dcb0af9cb024f
hjguang <houjguang@163.com>
2016-04-19 17:04:54 +0800

    modify test.java

--- src/pro/Test.java
+++ src/pro/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("...........");
-		System.out.println("...........");
+		System.out.println("22222222");
 		System.out.println("...........");
 		System.out.println("...........");
 	}
