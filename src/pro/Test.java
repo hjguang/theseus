af5c99189e9ec9979af439b1f0381985612fb630
hjguang <houjguang@163.com>
2016-04-20 11:39:58 +0800

    ddd

--- src/pro/Test.java
+++ src/pro/Test.java
@@ -3,7 +3,7 @@ package pro;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(".11....33333333333......");
+		System.out.println(".11....45678......");
 		System.out.println(".11......333....");
 		System.out.println("22222222");
 		System.out.println("...........");
