0155e6c8eeb1cd3c61d63460bd83cfb626b0308a
hjguang <houjguang@163.com>
2016-04-20 11:31:05 +0800

    ddd

---
+++ src/pro/Test.java
@@ -0,0 +1,12 @@
+package pro;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(".11....33333333333......");
+		System.out.println(".11......333....");
+		System.out.println("22222222");
+		System.out.println("...........");
+		System.out.println("11111111");
+	}
+}
