59d22a927bd08c6db061ed0b377bfd7d6324b5b3
hjguang <houjguang@163.com>
2016-04-20 10:40:42 +0800

    1111

--- src/pro/Test.java
+++ src/pro/Test.java
@@ -3,7 +3,7 @@ package pro;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("...........");
+		System.out.println(".3333..........");
 		System.out.println("22222222");
 		System.out.println("...........");
 		System.out.println("11111111");
